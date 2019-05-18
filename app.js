53e38d5e75d1acc7d04e4bc2c40f39665126d8cc
Shaun W Colley <swc7838@gmail.com>
2019-05-18 15:36:29 -0500

    fixed app.js

--- app.js
+++ app.js
@@ -9,14 +9,13 @@ app.use(bodyParser.json())
 
 models = require('./models')
 
-app.get('/', (req,res) => {
-  res.send("Hello")
-})
+
 app.get('/api/getData', (req, res) => {
-  models.NpReport.findAll().then(result => res.json(result))
-  console.log(result)
-  res.json({success: true, message:"Data is being populated..."})
+  models.NpReport.findAll().then(result => {
+  res.json({success: true, message:"Data is being populated...", result: result})
+}
 }).catch(error => res.json({success: false, message:"ERROR: Data could not be populated..."}))
+})
 
 
 app.post('/api/reading', (req,res) => {
