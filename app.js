aac3ca7bfc7c675f645e25b5c5710baf38050545
Shaun W Colley <swc7838@gmail.com>
2019-05-18 15:19:40 -0500

    commiting server for deploy

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const app = express()
+const PORT = process.env.PORT || 8080
+
+app.use(cors())
+app.use(bodyParser.json())
+
+models = require('./models')
+
+
+app.get('/api/getData', (req, res) => {
+  models.NpReport.findAll().then(result => res.json(result))
+  console.log(result)
+  res.json({success: true, message:"Data is being populated..."})
+}).catch(error => res.json({success: false, message:"ERROR: Data could not be populated..."}))
+
+
+app.post('/api/reading', (req,res) => {
+  let latitude = parseFloat(req.body.latitude)
+  let longitude = parseFloat(req.body.longitude)
+  let decibel = parseFloat(req.body.decibel)
+  let reading = models.NpReport.build({
+    latitude: latitude,
+    longitude: longitude,
+    decibel: decibel
+  })
+  reading.save().then(reading => {
+    res.json({success:true, message:"Decibel reading was saved to database."})
+  }).catch(error => {
+    res.json({sucess: false, message:"Reading could not be saved", error: error})
+  })
+})
+
+
+app.listen(PORT,function(){
+  console.log("Server is running...")
+})
